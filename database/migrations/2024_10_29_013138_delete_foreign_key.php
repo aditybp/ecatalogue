eb816c4817a667ac1c8911b1b6c96459ce22bd40
Aditya Bayu Putra Pratama <bayuaditya0111@gmail.com>
2024-10-29 08:39:39 +0700

    drop foreign key

---
+++ database/migrations/2024_10_29_013138_delete_foreign_key.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->dropForeign(['id_roles']);
+            $table->dropForeign(['satuan_kerja_id']);
+            $table->dropForeign(['balai_kerja_id']);
+        });
+
+        Schema::table('kategori_vendors', function (Blueprint $table) {
+            $table->dropForeign(['jenis_vendor_id']);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+    }
+};
