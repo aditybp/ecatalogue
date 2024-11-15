6127fde6293fd68a750c0d0ffa1439f3fba1e556
Aditya Bayu Putra Pratama <bayuaditya0111@gmail.com>
2024-11-15 09:18:53 +0700

    change bug save vendor

--- app/Http/Controllers/VendorController.php
+++ app/Http/Controllers/VendorController.php
@@ -9,38 +9,19 @@ use Illuminate\Support\Facades\Validator;
 class VendorController extends Controller
 {
 
-    public function allVendor(){
+    public function allVendor()
+    {
         return DataVendor::all();
     }
 
-    public function inputVendor(Request $request){
+    public function inputVendor(Request $request)
+    {
 
-        // $validator = Validator::make($request->all(), [
-        //     'nama_vendor' => 'required|string|max:255', 
-        //     'jenis_vendor_id' => 'required', 
-        //     'kategori_vendor_id' => 'required', 
-        //     'alamat' => 'required|string|max:255', 
-        //     'no_telepon' => 'required|string',
-        //     'no_hp' => 'required|integer',
-        //     'provinsi_id' => 'required',
-        //     'kota_id' => 'required',
-        //     'koordinat' => 'required|string',
-        //     'sumber_daya' => 'required|string'
-        // ]);
-
-        // if ($validator->fails()) {
-        //     return response()->json([
-        //         'status' => 'error',
-        //         'message' => 'validasi gagal!',
-        //         'data' => []
-        //     ]);
-        // }
-
-        try{
+        try {
             $vendor = new DataVendor();
             $vendor->nama_vendor = $request->nama_vendor;
-            $vendor->jenis_vendor_id = $request->jenis_vendor_id;
-            $vendor->kategori_vendor_id = $request->kategori_vendor_id;
+            $vendor->jenis_vendor_id = json_decode($request->jenis_vendor_id, true);
+            $vendor->kategori_vendor_id = json_decode($request->kategori_vendor_id, true);
             $vendor->alamat = $request->alamat;
             $vendor->no_telepon = $request->no_telepon;
             $vendor->no_hp = $request->no_hp;
@@ -58,14 +39,12 @@ class VendorController extends Controller
                 'message' => 'Data Vendor berhasil disimpan',
                 'data' => $vendor
             ]);
-
-        }catch(\Throwable $th){
+        } catch (\Throwable $th) {
             return response()->json([
                 'status' => 'error',
                 'message' => 'Gagal menyimpan data vendor',
                 'error' => $th->getMessage()
             ]);
         }
-        
     }
 }
